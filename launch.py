8faaeff419e293f8b7b5d32e92bd49e9f3968099
abhijato <68499000+abhijato@users.noreply.github.com>
2021-02-05 17:15:20 +0530

    filter draws

--- launch.py
+++ launch.py
@@ -12,26 +12,26 @@ def PlayGame():
         while not board.is_game_over():
             result = eng.play(board, engine.Limit(time=15))
             board.push(result.move)
-        game.add_line(board.move_stack)
-        game.headers["Event"]="Stockfish VS Stockfish(15s)"
-        game.headers["Site"]="Heroku's server"
-        game.headers["White"]="Stockfish"
-        game.headers["Black"]="Stockfish"
-        game.headers["Result"]=board.result()
-        exporter = pgn.StringExporter(headers=True, variations=True, comments=True)
-        pgn_string = game.accept(exporter)
-        print(pgn_string)
+        if not board.result=='1/2-1/2':
+            game.add_line(board.move_stack)
+            game.headers["Event"]="Stockfish VS Stockfish(15s)"
+            game.headers["Site"]="Heroku's server"
+            game.headers["White"]="Stockfish 12"
+            game.headers["Black"]="Stockfish 12"
+            game.headers["Result"]=board.result()
+            exporter = pgn.StringExporter(headers=True, variations=True, comments=True)
+            pgn_string = game.accept(exporter)
+            print(pgn_string)
+            port = 465  # For SSL
+            smtp_server = "smtp.gmail.com"
+            sender_email = "lichessbotall@gmail.com"  # Enter your address
+            receiver_email = "abhijato.chatterjee@gmail.com"  # Enter receiver address
+            password = "lichess bot api"
 
-        port = 465  # For SSL
-        smtp_server = "smtp.gmail.com"
-        sender_email = "lichessbotall@gmail.com"  # Enter your address
-        receiver_email = "abhijato.chatterjee@gmail.com"  # Enter receiver address
-        password = "lichess bot api"
-
-        context = ssl.create_default_context()
-        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-            server.login(sender_email, password)
-            server.sendmail(sender_email, receiver_email, pgn_string)
+            context = ssl.create_default_context()
+            with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+                server.login(sender_email, password)
+                server.sendmail(sender_email, receiver_email, pgn_string)
 
 
 if __name__=="__main__":
